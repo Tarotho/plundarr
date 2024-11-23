444ed4eff731306cd76f88ca84b5c35ed8a124ce
Tarotho <luis.alonso.cantero@gmail.com>
2024-11-23 02:01:32 +0100

    incluyendo un validador del series.yaml

--- main.py
+++ main.py
@@ -34,33 +34,31 @@ def main(config):
     downloaded_episodes = load_downloaded_episodes()  # Cargar los episodios ya descargados desde save.json
 
     wished_series_list = load_series_list()
+    if validate_series_yaml(wished_series_list):
+        for wished_series in wished_series_list:
+            playlists = wished_series["playlist"]  # Puede ser una lista o una URL
 
-    for wished_series in wished_series_list:
-        playlists = wished_series["playlist"]  # Puede ser una lista o una URL
+            for playlist_url in playlists:  # Descargamos cada una de las playlist usando su url
 
-        for playlist_url in playlists:  # Descargamos cada una de las playlist usando su url
+                playlist_info = get_playlist_info(playlist_url)  # Obtener la información de la lista de reproducción
 
-            playlist_info = get_playlist_info(playlist_url)  # Obtener la información de la lista de reproducción
-
-            for video_information in playlist_info.get("entries"):
-                title_video = video_information['title']
-                if is_episode_downloaded(title_video, downloaded_episodes):  # Verificar si ya se descargó este video
-                    print(f"El capitulo {title_video} ya fué descargado con anterioridad, saltando.")
-                    continue
-                print(f'Se procede a descargar {title_video}')
-                episode_information = generate_episode_information(video_information, wished_series)
-                telegram = activate_telegram(config.get('telegram'))
-                download_video(episode_information, downloaded_episodes, telegram)
+                for video_information in playlist_info.get("entries"):
+                    title_video = video_information['title']
+                    if is_episode_downloaded(title_video,
+                                             downloaded_episodes):  # Verificar si ya se descargó este video
+                        print(f"El capitulo {title_video} ya fué descargado con anterioridad, saltando.")
+                        continue
+                    print(f'Se procede a descargar {title_video}')
+                    episode_information = generate_episode_information(video_information, wished_series)
+                    telegram = activate_telegram(config.get('telegram'))
+                    download_video(episode_information, downloaded_episodes, telegram)
 
 
 if __name__ == "__main__":
     shutil.move("data/series.yaml", "config/series.yaml")
     while True:  # Ciclo infinito
         config_file = load_config()  # Vuelve a cargar la configuración en cada ciclo
-        if validate_series_yaml(config_file):
-            main(config_file)
-            download_interval = config_file.get("download_interval", 60)
-            print(f"Esperando {download_interval} minutos antes de la siguiente ejecución...")
-            time.sleep(download_interval * 60)  # Dormir por el intervalo (convertido a segundos)
-        else:
-            time.sleep(600)  # esperar 10 minutos y volver a intentar
+        main(config_file)
+        download_interval = config_file.get("download_interval", 60)
+        print(f"Esperando {download_interval} minutos antes de la siguiente ejecución...")
+        time.sleep(download_interval * 60)  # Dormir por el intervalo (convertido a segundos)
